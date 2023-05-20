b7ca8ce902033f643b15c35443f6b13dd069a836
Dibanisar <134138349+Dibanisar@users.noreply.github.com>
2023-05-20 13:54:33 +0200

    Add files via upload

---
+++ data/Reprojected_2.js
@@ -0,0 +1,10 @@
+var json_Reprojected_2 = {
+"type": "FeatureCollection",
+"name": "Reprojected_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "gid": 1.0, "compnt_nm": "HARARE", "location_x": 18.6693, "location_y": -34.05193, "create_dt": "20221122", "version": "1.2.7" }, "geometry": { "type": "Point", "coordinates": [ 18.669300000294811, -34.051930000169307 ] } },
+{ "type": "Feature", "properties": { "gid": 2.0, "compnt_nm": "LINGELETHU-WEST", "location_x": 18.66004, "location_y": -34.04298, "create_dt": "20221122", "version": "1.2.7" }, "geometry": { "type": "Point", "coordinates": [ 18.66004000044569, -34.042980000226805 ] } },
+{ "type": "Feature", "properties": { "gid": 3.0, "compnt_nm": "KHAYELITSHA", "location_x": 18.66599, "location_y": -34.02464, "create_dt": "20221122", "version": "1.2.7" }, "geometry": { "type": "Point", "coordinates": [ 18.665989999638839, -34.024639999836097 ] } }
+]
+}
